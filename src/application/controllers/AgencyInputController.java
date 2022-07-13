d0d84e3b284c5f491b1f51e536ee3152e59fbbc4
Jeff <jeffersoncerqueira75@gmail.com>
2022-07-13 14:09:27 -0300

    adicionando validaçoes

--- src/application/controllers/AgencyInputController.java
+++ src/application/controllers/AgencyInputController.java
@@ -14,10 +14,13 @@ import application.classes.Vinheta;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class AgencyInputController implements Initializable {
@@ -54,6 +57,9 @@ public class AgencyInputController implements Initializable {
     @FXML
     private CheckBox especialInput;
 
+    @FXML
+    private Button agencyRegisterButton;
+
 
     @FXML
     public void closeAgencyInput() throws IOException{
@@ -65,22 +71,50 @@ public class AgencyInputController implements Initializable {
     @FXML
     public void agencyRegister(ActionEvent e) throws IOException{
         Adress adress = new Adress(adressInputPlace.getText(), Integer.parseInt(adressInputNumber.getText()), adressInputPostal.getText());
+        System.out.println(managerChoice.getValue());
 
-        if(especialInput.isSelected()){
-            Vinheta vinheta = new Vinheta(campaingInputDescription.getText(), Double.parseDouble(campaingInputValue.getText()), Integer.parseInt(campaignInputCode.getText()), Double.parseDouble(vinhetaInputTime.getText()));
-            EspecialAgency especialAgency = new EspecialAgency(agencyInputName.getText(), agencyInputCNPJ.getText(), adress, App.managerBank.search(managerChoice.getValue()), vinheta);
+        if(managerChoice.getValue() == null){
+            System.out.println("entrei no alert");
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setTitle("Manager Error");
+            alert.setHeaderText("Please select a manager to you agency");
+            alert.setContentText("If you don't have registry a manager, please do it on the previous screen");
+            alert.showAndWait();
+        }else if(false){}else{
 
-            App.agencyBank.insert(especialAgency);
-            System.out.println(especialAgency);
-        }else{
-            Commum commum = new Commum(campaingInputDescription.getText(), Double.parseDouble(campaingInputValue.getText()), Integer.parseInt(campaignInputCode.getText()));
-            CommumAgency commumAgency = new CommumAgency(agencyInputName.getText(), agencyInputCNPJ.getText(), adress, App.managerBank.search(managerChoice.getValue()), commum);
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.getButtonTypes().remove(1);
+            alert.setTitle("Register Confirmation");
+            alert.setHeaderText("Your register has been confirmed");
+            alert.showAndWait();
 
-            App.agencyBank.insert(commumAgency);
+            try {
+                if(especialInput.isSelected()){
+                    Vinheta vinheta = new Vinheta(campaingInputDescription.getText(), Double.parseDouble(campaingInputValue.getText()), Integer.parseInt(campaignInputCode.getText()), Double.parseDouble(vinhetaInputTime.getText()));
+                    EspecialAgency especialAgency = new EspecialAgency(agencyInputName.getText(), agencyInputCNPJ.getText(), adress, App.managerBank.search(managerChoice.getValue()), vinheta);
+        
+                    App.agencyBank.insert(especialAgency);
+                    System.out.println(especialAgency);
+                }else{
+                    Commum commum = new Commum(campaingInputDescription.getText(), Double.parseDouble(campaingInputValue.getText()), Integer.parseInt(campaignInputCode.getText()));
+                    CommumAgency commumAgency = new CommumAgency(agencyInputName.getText(), agencyInputCNPJ.getText(), adress, App.managerBank.search(managerChoice.getValue()), commum);
+        
+                    App.agencyBank.insert(commumAgency);
+                }
+
+                Stage agencyInput = ConfigController.getAgencyInput();
+                agencyInput.close(); //fecha ao registrar
+
+            }catch(Exception erro){
+                Alert alertError = new Alert(AlertType.ERROR);
+                alertError.showAndWait();
+            }
+           
+    
+            
         }
 
-        Stage agencyInput = ConfigController.getAgencyInput();
-        agencyInput.close(); //fecha ao registrar
+        
         
     }
 
@@ -90,6 +124,16 @@ public class AgencyInputController implements Initializable {
             vinhetaInputTime.setDisable(false);
         }else {
             vinhetaInputTime.setDisable(true);
+        }
+    }
+
+    @FXML void registerToggle(ActionEvent e){
+        System.out.println("entrei no register toggle");
+        if (managerChoice.getValue() == ""){
+            agencyRegisterButton.setDisable(false);
+        }
+        else {
+            agencyRegisterButton.setDisable(false);
         }
     }
 
--- src/application/controllers/ShowController.java
+++ src/application/controllers/ShowController.java
@@ -3,6 +3,7 @@ package application.controllers;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
 
 import application.App;
 import application.classes.Agency;
@@ -57,15 +58,54 @@ public class ShowController implements Initializable{
     private TextField agencyInputName;
 
 
-
     private infoBar[] transformItensInParents(Agency[] a) throws IOException{
         infoBar[] returnParents = new infoBar[App.agencyBank.getBankLenght()];
 
-        showArea.setOnMouseClicked(new EventHandler<Event>() {
+        this.showArea.setOnMouseClicked(new EventHandler<Event>() {
             
             @Override
             public void handle(Event event){
                 indexSelected = showArea.getSelectionModel().getSelectedIndex();
+                try {
+                    this.showAgency();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            private void showAgency() throws IOException {
+                Agency a = App.agencyBank.getBankItens()[indexSelected];
+        
+                    name = a.getName();
+                    CNPJ = a.getCNPJ();
+                    publicplace = a.getAdress().getPlace();
+                    number = String.valueOf(a.getAdress().getNumber()); 
+        
+                    if (a.getManager().getName() == null){
+                        manager = "";
+                    }else{
+                        manager = a.getManager().getName();
+                    }
+        
+        
+                    postalCode = a.getAdress().getCode();
+                    especial = a.getCampaign().getTime() != "";
+                    description = a.getCampaign().getDescription();
+                    value = a.getCampaign().getValue();
+                    code = a.getCampaign().getCode();
+                    time = a.getCampaign().getTime();
+        
+        
+                Stage stage = new Stage();
+                FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../../Scenes/AgencyShow.fxml"));
+                Pane root = loader.load();
+        
+                
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+        
             }
         });
        
@@ -77,13 +117,8 @@ public class ShowController implements Initializable{
             label.setTextFill(Color.WHITE);
             label.setFont(Font.font("System", 15));
             root.getChildren().set(1, label);
-            //returnParents[i] = root;
             infoBar bar = new infoBar(i, root, a[i]);
             returnParents[i] = bar;
-            
-
-            System.out.println(root.getChildren().get(1));
-
         }
 
         return returnParents;
@@ -96,57 +131,6 @@ public class ShowController implements Initializable{
         for (int i = 0; i < bankItens2.length; i++){
             showArea.getItems().add(bankItens2[i].getParent());
         }
-
-        
-
-        System.out.println(showArea);
-        System.out.println(showArea.getSelectionModel().getSelectedIndex());
-
-
-    }
-
-    @FXML
-    public void updateSelectedItem() {
-        System.out.println("Estou no update Selected Item");
-        System.out.println(indexSelected);
-        System.out.println(showArea);
-        //indexSelected = this.showArea.getSelectionModel().getSelectedIndex();
-    }
-
-    @FXML
-    public void showAgency(Event e)throws IOException, InterruptedException{
-
-        Agency a = App.agencyBank.getBankItens()[indexSelected];
-
-            name = a.getName();
-            CNPJ = a.getCNPJ();
-            publicplace = a.getAdress().getPlace();
-            number = String.valueOf(a.getAdress().getNumber()); 
-
-            if (a.getManager().getName() == null){
-                manager = "";
-            }else{
-                manager = a.getManager().getName();
-            }
-
-
-            postalCode = a.getAdress().getCode();
-            especial = a.getCampaign().getTime() != "";
-            description = a.getCampaign().getDescription();
-            value = a.getCampaign().getValue();
-            code = a.getCampaign().getCode();
-            time = a.getCampaign().getTime();
-
-
-        Stage stage = new Stage();
-        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../../Scenes/AgencyShow.fxml"));
-        Pane root = loader.load();
-
-        
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-
     }
 
 
